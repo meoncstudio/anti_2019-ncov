2988b625e8d5575da76d0a29647fc526c933347d
Cloudy Young <cloudyyoung74@outlook.com>
2020-01-27 03:17:55 -0700

    update ui, responsive view

--- public/oapi/sta.php
+++ public/oapi/sta.php
@@ -1,6 +1,8 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+    
     $json = file_get_contents("sta.json");
     
     $data = json_decode ($json, true);
