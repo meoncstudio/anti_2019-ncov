133c38b6b81d707d244c7f15cf4626cdf61f2f6b
Cloudy Young <cloudyyoung74@outlook.com>
2020-01-27 00:05:59 -0700

    ui polished
    Spectre.css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Spectre from 'spectre.css';
 import router from "./router";
 import store from "./store";
 
-Vue.use(ElementUI);
+
+Vue.use(Spectre);
 
 Vue.config.productionTip = false;
 
