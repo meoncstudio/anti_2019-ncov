a6111039e08abf6639b88df5e32fa76571050a49
Cloudy Young <cloudyyoung74@outlook.com>
2020-01-27 03:45:07 -0700

    Update sta.php

--- public/api/sta.php
+++ public/api/sta.php
@@ -1,15 +1,13 @@
 <?php
-    // ini_set('display_errors', 1);
-    // ini_set('display_startup_errors', 1);
-    // error_reporting(E_ALL);
-    display_errors = On
-    error_reporting = E_ALL | E_STRICT
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
     
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
-    $rst = $_REQUEST['rst']；
+    $rst = $_REQUEST['rst'];
 
     $json = file_get_contents("sta.json");
     
